5525eca83870b006b6f780f0b88da728436b3a25
NateBu <you@example.com>
2018-06-04 22:21:13 +0000

    test3

--- tools/gdb8.1.sh
+++ tools/gdb8.1.sh
@@ -5,7 +5,7 @@ tar -xf gdb-8.1.tar.xz
 cd gdb-8.1/
 ./configure --prefix=$DIR --with-python=/usr/bin/python3 --with-system-readline --without-guile && make -j8
 make -C gdb install
-# cp -r gdb/data-directory/* ..
+#cp -r gdb/data-directory/* ..
 cd ..
 rm -r gdb-8.1
 rm -r gdb-8.1.tar.gz
