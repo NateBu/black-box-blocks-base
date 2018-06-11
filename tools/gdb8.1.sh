5c0cd7c0f3f120bfb29b73eb890be05d2da6f799
NateBu <you@example.com>
2018-06-11 22:21:39 +0000

    fixed gdb

--- tools/gdb8.1.sh
+++ tools/gdb8.1.sh
@@ -1,11 +1,7 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-apt-get install -y texinfo wget libreadline-dev
-wget http://ftp.gnu.org/gnu/gdb/gdb-8.1.tar.xz
-tar -xf gdb-8.1.tar.xz
-cd gdb-8.1/
-./configure --prefix=$DIR --with-python=/usr/bin/python3 --with-system-readline --without-guile && make -j8
-make -C gdb install
-#cp -r gdb/data-directory/* ..
-cd ..
-rm -r gdb-8.1
-rm -r gdb-8.1.tar.xz
+RUN apt-get install -y texinfo wget libreadline-dev
+RUN wget http://ftp.gnu.org/gnu/gdb/gdb-8.1.tar.xz
+RUN tar -xf gdb-8.1.tar.xz
+WORKDIR gdb-8.1/
+RUN ./configure --prefix=/usr/local --with-python=/usr/bin/python3 --with-system-readline --without-guile && make -j8
+RUN make -C gdb install
+RUN cd .. && rm -r gdb-8.1 && rm -r gdb-8.1.tar.xz
