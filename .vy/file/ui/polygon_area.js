ca2e91564415e802156391a2f9a4884bb7e899ba
NateBu <nbunderson@gatech.edu>
2018-09-13 01:29:03 -0600

    Slight reformat

---
+++ .vy/file/ui/polygon_area.js
@@ -0,0 +1,8 @@
+signed_area = function(vertices) {
+  var area = 0;
+  for (var ii=0; ii<vertices.length; ii++) {
+    var jj = (ii+1) % vertices.length;
+    area += (vertices[ii].x*vertices[jj].y - vertices[jj].x*vertices[ii].y);
+  }
+  return area/2;
+};
