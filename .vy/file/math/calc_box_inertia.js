9dc446f288e5bb43c4a86d3e6d6adbe4d3d02306
NateBu <nbunderson@gatech.edu>
2018-09-13 01:13:25 -0600

    test

--- .vy/file/math/calc_box_inertia.js
+++
@@ -1,7 +0,0 @@
-calc_box_inertia = function(dx,dy,dz,density) {
-  var mass = density*dx*dy*dz;
-  var Ix = (dy*dy+dz*dz)*mass/12;
-  var Iy = (dx*dx+dz*dz)*mass/12;
-  var Iz = (dy*dy+dx*dx)*mass/12;
-  return {mass:mass,Ix:Ix,Iy:Iy,Iz:Iz};
-};
